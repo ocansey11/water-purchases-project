a159525c457eada31fc837a06704b778a8b3bc6c
kevin ocansey <kevinocansey11@gmail.com>
2023-09-20 15:14:15 +0000

    Solved a DB connection issues. I changed localhost to 127.0.0.1 and removed the client.close after try block which connects the water-user db

--- db.js
+++ db.js
@@ -1,29 +1,55 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-// Replace the uri string with your connection string.
-const uri = "mongodb://localhost:27017/";
+// Replace the placeholder with your Atlas connection string
+const uri = "mongodb://127.0.0.1:27017/water-users";
 
-const client = new MongoClient(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-const db = client.db();
 async function run() {
   try {
-    const database = client.db("water-purchases");
-    const purchases = database.collection("purchases");
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
 
-    //learn queries
-    const cursor = purchases.find({ Package: "Bottle" });
-
-    console.log(cursor);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (e) {
+    // Ensures that the client ccatches/error
+    console.log(e);
   }
 }
-run().catch(console.dir);
+
+async function addUser(username, email, hashedPassword) {
+  try {
+    const usersDb = client.db("water-users");
+    const usersCollection = usersDb.collection("users");
+    //learn queries
+    const newUser = {
+      username,
+      email,
+      hashedPassword,
+    };
+
+    const result = await usersCollection.insertOne(newUser);
+
+    // Connection management is handled outside this function
+  } catch (error) {
+    console.error("Error adding user:", error);
+    // You can choose to handle errors here or let them propagate to the caller
+    throw error;
+  }
+}
 
 module.exports = {
   run,
-  uri,
-  client,
+  addUser,
 };
